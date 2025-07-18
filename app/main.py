c6ecff87c4c7b6a566f2ec8ce8627fa57d997b03
Ariane Agboton <agbotonariane70@gmail.com>
2025-07-18 11:38:57 +0100

    Préparation pour le déploiement

--- app/main.py
+++ app/main.py
@@ -916,3 +916,6 @@ async def get_me_admin(current_user: dict = Depends(get_current_user)):
 
 #------------------ End -------------------#
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
